92bae9f179ef422758d9c7250379d5b50133f7da
baibo <524027223@qq.com>
2019-10-26 22:07:44 +0800

    bo

---
+++ testjsch/src/com/bob/smalltool/TestSshSendCommand.java
@@ -0,0 +1,18 @@
+package com.bob.smalltool;
+
+/**
+ * @program: testjsch
+ * @description: jsh测试类
+ * @author: Mr.Bob
+ * @create: 2019-10-26 21:48
+ **/
+public class TestSshSendCommand {
+    public static void main(String[] args) {
+
+    }
+
+    public  boolean  creatConnection(){
+       return false;
+    }
+
+}
